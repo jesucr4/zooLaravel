c15e0c6b50448b2622db92882ea6c79a16c541b6
jesucr4 <jesucr4@users.noreply.github.com>
2021-03-08 12:35:39 +0100

    proyecto final

---
+++ resources/views/mantenimiento.blade.php
@@ -0,0 +1,2 @@
+<img src="https://neliosoftware.com/es/wp-content/uploads/sites/3/2016/04/modo-mantenimiento.png" style="margin-left: 20%">
+<a href="{{ route('login') }}">Volver a pantalla principal</a>
